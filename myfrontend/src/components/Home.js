a5e95b702a4bd5090db0ccf3e080d31ddbfe9e55
Bill <jinningzhang6@gmail.com>
2020-10-15 21:33:41 -0700

    Added ReactJS code

---
+++ myfrontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Home extends React.Component{
+
+
+  render(){
+    return(
+      <h1>Welcome Home!</h1>
+    );
+  }
+}
+
+export default Home;
---
+++ myfrontend/src/components/MenuItems.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+export const MenuItems=[
+  {
+    title: 'Home',
+    url: '/',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Services',
+    url: 'Services',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Statements',
+    url: 'Statements',
+    cName: 'nav-links'
+  }
+
+]
