cf6d3684ee7b8a859e7d87229bd1860f203d9e2a
littleflute <littleflute@163.com>
2020-12-01 13:12:02 -0800

    *

--- node/old/main/index.js
+++ node/old/main/index.js
@@ -1,12 +1,11 @@
-const tag = "[main/index.js_v0.0.22] ";
+const tag = "[main/index.js_v0.0.23] ";
 
 const express = require('express')
 const app = express()
 const ut = require('../utils/test');
 const db = require('../utils/db');
 
-const l = require('../../logger');
-const db = require('../utils/db');
+const l = require('../../logger'); 
 l.tag(tag + __dirname); 
 
 const port = 8080
