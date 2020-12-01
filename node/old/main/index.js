6817f1acc5f51b3bc013c530d38ed539ba7f8ec5
littleflute <littleflute@163.com>
2020-12-01 11:53:44 -0800

    main/index.js*

--- node/old/main/index.js
+++ node/old/main/index.js
@@ -1,5 +1,10 @@
+const tag = "[main/index.js_v0.0.12] ";
+
 const express = require('express')
 const app = express()
+const l = require('../../logger');
+l.tag(tag + __dirname); 
+
 const port = 8080
 
 app.get('/', (req, res) => {
