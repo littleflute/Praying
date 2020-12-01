f5961c1d9141d796d711d9f7412623ca636a6db1
littleflute <littleflute@163.com>
2020-12-01 13:22:50 -0800

    config.js*

--- node/old/utils/db.js
+++ node/old/utils/db.js
@@ -1,4 +1,4 @@
-const tag = "[utils/db.js_v0.0.14]";
+const tag = "[utils/db.js_v0.0.15]";
 var mysql = require('mysql');
 const cfg = require('../config/config');
 const l = require('../../logger');
@@ -9,7 +9,7 @@ const db = {};
 db.test = function(){
     var r = "db.test...";
     var ci = cfg.getDbConInf();
-    r += JSON.stringify(i);
+    r += JSON.stringify(ci);
 
     var con = mysql.createConnection(ci);
 
